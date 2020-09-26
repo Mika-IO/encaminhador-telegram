a78b7e2d0febff1b528c065b3bfe5587d0f7ccc1
Mika-IO <mikaio4321@gmail.com>
2020-09-25 21:43:12 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+# Token do usuário 
+TOKEN_USER = '1016353473_6802340938325777775'
+# Token do chat do grupo remetente
+CHAT_ID_SENDER = ''
+# Token do chat do grupo destinatário
+CHAT_ID_RECEIVER = ''
